f8f47d0853596c0d45b43ed07bcd3e28df86a795
Joe Hand <joe@joeahand.com>
2016-09-06 15:01:15 -0700

    fix options on tests

--- tests/misc.js
+++ tests/misc.js
@@ -5,7 +5,7 @@ var Dat = require('..')
 
 test('default ignore', function (t) {
   var dat = Dat({ dir: process.cwd() })
-  var matchers = dat.opts.ignore
+  var matchers = dat.options.ignore
   t.ok(anymatch(matchers, '.dat'), '.dat folder ignored')
   t.ok(anymatch(matchers, '.dat/'), '.dat folder with slash ignored')
   t.ok(anymatch(matchers, '.dat/foo.bar'), 'files in .dat folder ignored')
@@ -16,9 +16,9 @@ test('default ignore', function (t) {
 
 test('custom ignore extends default (string)', function (t) {
   var dat = Dat({ ignore: '**/*.js', dir: process.cwd() })
-  var matchers = dat.opts.ignore
+  var matchers = dat.options.ignore
 
-  t.ok(Array.isArray(dat.opts.ignore), 'ignore extended correctly')
+  t.ok(Array.isArray(dat.options.ignore), 'ignore extended correctly')
   t.ok(anymatch(matchers, '.dat'), '.dat folder ignored')
   t.ok(anymatch(matchers, 'foo/bar.js'), 'custom ignore works')
   t.notOk(anymatch(matchers, 'foo/bar.txt'), 'txt file gets to come along =)')
@@ -27,9 +27,9 @@ test('custom ignore extends default (string)', function (t) {
 
 test('custom ignore extends default (array)', function (t) {
   var dat = Dat({ ignore: ['super_secret_stuff/*', '**/*.txt'], dir: process.cwd() })
-  var matchers = dat.opts.ignore
+  var matchers = dat.options.ignore
 
-  t.ok(Array.isArray(dat.opts.ignore), 'ignore extended correctly')
+  t.ok(Array.isArray(dat.options.ignore), 'ignore extended correctly')
   t.ok(anymatch(matchers, '.dat'), '.dat still feeling left out =(')
   t.ok(anymatch(matchers, 'password.txt'), 'file ignored')
   t.ok(anymatch(matchers, 'super_secret_stuff/file.js'), 'secret stuff stays secret')
