d71382c36d6847764e02ee9c42a82104373f7008
Yoshua Wuyts <yoshuawuyts@gmail.com>
2016-10-12 15:10:01 +0200

    fix(share): download instead of warn
    Start seeding a file rather than warning the user they should seed the
    file
    - ref: https://github.com/joehand/dat-js/issues/15

--- index.js
+++ index.js
@@ -100,8 +100,7 @@ Dat.prototype.share = function (cb) {
     if (err) return cb(err)
 
     if (archive.key && !archive.owner) {
-      // TODO: allow this but change to download
-      return cb('Dat previously downloaded. Run dat ' + encoding.encode(archive.key) + ' to resume')
+      return self.download()
     }
 
     if ((archive.live || archive.owner) && archive.key) {
