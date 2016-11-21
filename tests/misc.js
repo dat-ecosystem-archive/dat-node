3be8ed748a21b82f98d29b91cc280b8ae54365b0
Julian Gruber <julian@juliangruber.com>
2016-11-21 19:28:24 +0100

    split out and simplify live .key test

--- tests/misc.js
+++ tests/misc.js
@@ -52,11 +52,20 @@ test('custom ignore extends default (array)', function (t) {
 test('custom db option', function (t) {
   var dat = Dat({db: memdb(), dir: process.cwd()})
   dat.open(function (err) {
-    t.ok(dat.archive instanceof require('hyperdrive/archive'), 'archive is hyperdrive archive')
-    t.deepEqual(dat.archive.key, dat.key)
     t.error(err)
     t.ok(dat.db.db instanceof require('memdown'), 'db is memdown')
 
+    dat.close(function () {
+      t.end()
+    })
+  })
+})
+
+test('.key on live archive', function (t) {
+  var dat = Dat({db: memdb(), dir: process.cwd()})
+  dat.open(function (err) {
+    t.error(err)
+    t.deepEqual(dat.key, dat.archive.key)
     dat.close(function () {
       t.end()
     })
