c142a080b5000bbe3cf1fb0deef91c56b410160c
Joe Hand <joe@joeahand.com>
2017-01-16 10:17:43 -0800

    add debug statements (#74)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var assert = require('assert')
 var path = require('path')
 var untildify = require('untildify')
 var multicb = require('multicb')
+var debug = require('debug')('dat-node')
 var initArchive = require('./lib/init-archive')
 var importFiles = require('./lib/import-files')
 var network = require('./lib/network')
@@ -20,9 +21,12 @@ module.exports = function (dir, opts, cb) {
     path: dir,
     options: opts
   }
-
+  debug('Running initArchive on', dir, 'with opts:', opts)
   initArchive(dir, opts, function (err, archive, db) {
     if (err) return cb(err)
+
+    debug(`${archive.resumed ? 'Resumed' : 'Created'} archive ${archive.key && archive.key.toString('hex')}`)
+    debug(`Live: ${archive.live}, Owner: ${archive.owner}`)
 
     dat.archive = archive
     dat.db = db
@@ -32,6 +36,7 @@ module.exports = function (dir, opts, cb) {
     dat.resumed = archive.resumed
 
     dat.joinNetwork = function (opts) {
+      debug('Joining network with opts:', opts)
       dat.network = network(archive, opts)
       dat.options.network = dat.network.options
       if (dat.owner) return dat.network
@@ -47,12 +52,14 @@ module.exports = function (dir, opts, cb) {
     }
 
     dat.trackStats = function () {
+      debug('Tracking Stats')
       dat.stats = stats(archive, db)
       return dat.stats
     }
 
     if (archive.owner) {
       dat.importFiles = function (opts, cb) {
+        debug('Importing Files with opts:', opts)
         if (typeof opts === 'function') return dat.importFiles({}, opts)
         if (archive.live) {
           dat.importer = importFiles(archive, dir, opts, cb)
