53bb7963d35ffec01bf3efe7a86e00ffae5970f2
Joe Hand <joe@joeahand.com>
2017-02-23 14:04:38 -0800

    Error on close twice sync (#114)
    * set _closed before sync, closes #85
    
    * test for close twice sync, #85
    
    * make sure test ends after close is done

--- test/misc.js
+++ test/misc.js
@@ -143,6 +143,20 @@ test('close twice errors', function (t) {
   })
 })
 
+test('close twice sync errors', function (t) {
+  Dat(shareFolder, function (err, dat) {
+    t.ifError(err)
+    dat.close(function (err) {
+      t.ifError(err)
+      rimraf.sync(path.join(shareFolder, '.dat'))
+      t.end()
+    })
+    dat.close(function (err) {
+      t.ok(err, 'has close error second time')
+    })
+  })
+})
+
 test('create key and open with different key', function (t) {
   rimraf.sync(path.join(shareFolder, '.dat'))
   Dat(shareFolder, function (err, dat) {
