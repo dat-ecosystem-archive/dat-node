a86d5a84172009c0eb1d57b7282264a0d5824dae
Joe Hand <joe@hand.email>
2017-04-13 12:39:13 -0700

    add helpers

---
+++ test/helpers/index.js
@@ -0,0 +1,44 @@
+var countFiles = require('count-files')
+
+var fixtureStats = {
+  files: 2,
+  bytes: 1441,
+  dirs: 1
+}
+
+module.exports.verifyFixtures = function (t, archive, cb) {
+  var pending = 4
+
+  archive.stat('/table.csv', function (err, stat) {
+    if (err) return cb(err)
+    t.same(stat.size, 1441, 'stat size ok')
+    if (!--pending) return done()
+  })
+
+  archive.stat('/folder/empty.txt', function (err, stat) {
+    if (err) return cb(err)
+    t.same(stat.size, 0, 'stat size ok')
+    if (!--pending) return done()
+  })
+
+  archive.readdir('/', function (err, entries) {
+    if (err) return cb(err)
+    t.ok(entries.indexOf('table.csv') > -1, 'csv in archive')
+    t.ok(entries.indexOf('folder') > -1, 'sub dir in archive')
+    if (!--pending) return done()
+  })
+
+  archive.readdir('/folder', function (err, entries) {
+    if (err) return cb(err)
+    t.ok(entries.indexOf('empty.txt') > -1, 'has empty file')
+    if (!--pending) return done()
+  })
+
+  function done () {
+    countFiles({fs: archive, name: '/'}, function (err, count) {
+      if (err) return cb(err)
+      t.same(count, fixtureStats, 'archive stats are correct')
+      cb()
+    })
+  }
+}
