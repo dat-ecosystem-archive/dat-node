c2ffc3bb5ece2370d5992aac5f5db91497aa09eb
Joe Hand <joe@hand.email>
2017-04-13 11:18:05 -0700

    add downloader to example

---
+++ examples/readme.md
@@ -0,0 +1,19 @@
+# Run Example
+
+The example will import all the files in the repo and then download them.
+
+## Share
+
+```
+node examples/share.js
+```
+
+Copy key printed.
+
+## Download
+
+```
+node examples/download.js <key>
+```
+
+This will download all files to `examples/tmp`. If not something may be broken.
--- examples/share.js
+++ examples/share.js
@@ -6,17 +6,20 @@ var src = path.join(__dirname, '..')
 Dat(src, {temp: true}, function (err, dat) {
   if (err) throw err
 
+  var network = dat.joinNetwork()
+  network.once('connection', function () {
+    console.log('Connected')
+  })
   var progress = dat.importFiles(src, {
     ignore: ['**/dat-node/node_modules/**']
   }, function (err) {
     if (err) throw err
     console.log('Done importing')
+    console.log('Archive size:', dat.archive.content.byteLength)
   })
   progress.on('put', function (src, dest) {
-    console.log('Added', src.name)
+    console.log('Added', dest.name)
   })
 
-  dat.joinNetwork()
-  dat.trackStats()
   console.log(`Sharing: ${dat.key.toString('hex')}\n`)
 })
